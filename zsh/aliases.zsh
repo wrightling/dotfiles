74e3876b1be2a1adc27fdc78f1ed9164aefa7834
wrightling <warren@thewrightangle.com>
2016-10-21 15:44:02 -0700

    add some aliases to jump to typical dev folders

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,3 +11,8 @@ alias m="open -a marked.app"
 alias be="bundle exec"
 
 alias dc="docker-compose -f docker-compose.dev.yml"
+
+alias clients="c web/clients"
+alias frontend="c web/frontend"
+alias services="c web/services"
+alias shared="c web/shared"
